be5c6a272a505ac48f406f819c5292fa6573e914
Alexander Dimitrash <fireboard@me.com>
2023-04-06 20:55:20 +0300

    Добавил 2 задачу

---
+++ Seminar4/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        Console.WriteLine($"Сумма цифр в числе равна {sum}");
+    }
+}
+
