9760082ab1ae61d658947e53c1b02253d86e7412
Alexander Dimitrash <fireboard@me.com>
2023-04-13 21:46:24 +0300

    Добавил решение задач

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Введите количество чисел: ");
+        int m = int.Parse(Console.ReadLine());
+        
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            Console.Write($"Введите число {i + 1}: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num > 0) {
+                count++;
+            }
+        }
+        
+        Console.WriteLine($"Количество чисел больше нуля: {count}");
+    }
+}
+
