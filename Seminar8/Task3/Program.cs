b0c08fb7ab5eb25852696c821582df8c00535ee2
Aleksandr Dimitrash <fireboard@me.com>
2023-04-16 17:05:39 +0300

    Разместил задание по 8 семинару

--- Seminar8/Task3/Program.cs
+++ Seminar8/Task3/Program.cs
@@ -1,16 +1,56 @@
-﻿double[] arr = {1.2, 3.4, 5.6, 7.8, 9.0};
-double min = arr[0];
-double max = arr[0];
-for(int i = 1; i < arr.Length; i++)
+﻿using System;
+
+class Program
 {
-    if(arr[i] < min)
+    static void Main(string[] args)
     {
-        min = arr[i];
-    }
-    else if(arr[i] > max)
-    {
-        max = arr[i];
+        // Задаем две матрицы размером 2x3 и 3x2
+        int[,] matrix1 = new int[,]
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 }
+        };
+
+        int[,] matrix2 = new int[,]
+        {
+            { 7, 8 },
+            { 9, 10 },
+            { 11, 12 }
+        };
+
+        // Проверяем, что количество столбцов первой матрицы равно количеству строк второй матрицы
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+        {
+            Console.WriteLine("Невозможно перемножить матрицы!");
+            return;
+        }
+
+        // Создаем новую матрицу размером с количество строк первой матрицы и количество столбцов второй матрицы
+        int[,] result = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+        // Вычисляем произведение матриц
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix2.GetLength(1); j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < matrix1.GetLength(1); k++)
+                {
+                    sum += matrix1[i, k] * matrix2[k, j];
+                }
+                result[i, j] = sum;
+            }
+        }
+
+        // Выводим результат
+        Console.WriteLine("Результат произведения матриц:");
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                Console.Write(result[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
-double diff = max - min;
-Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {diff}");
--- Seminar8/Task4/Program.cs
+++ Seminar8/Task4/Program.cs
@@ -1,16 +1,67 @@
-﻿double[] arr = {1.2, 3.4, 5.6, 7.8, 9.0};
-double min = arr[0];
-double max = arr[0];
-for(int i = 1; i < arr.Length; i++)
+﻿using System;
+
+class Program
 {
-    if(arr[i] < min)
+    static void Main(string[] args)
     {
-        min = arr[i];
-    }
-    else if(arr[i] > max)
-    {
-        max = arr[i];
+        int n = 5; // Размерность квадратной матрицы
+
+        int[,] matrix = new int[n, n]; // Создаем новую матрицу
+
+        int value = 1; // Начальное значение элемента матрицы
+
+        int row = 0; // Индекс строки текущего элемента
+        int col = 0; // Индекс столбца текущего элемента
+
+        int rowMax = n - 1; // Максимальный индекс строки
+        int colMax = n - 1; // Максимальный индекс столбца
+
+        int rowMin = 1; // Минимальный индекс строки
+        int colMin = 0; // Минимальный индекс столбца
+
+        while (value <= n * n) // Продолжаем заполнение, пока не достигнем конца матрицы
+        {
+            // Заполняем элементы верхней строки слева направо
+            for (int i = col; i <= colMax; i++)
+            {
+                matrix[row, i] = value;
+                value++;
+            }
+            row++; // Переходим на следующую строку
+
+            // Заполняем элементы правого столбца сверху вниз
+            for (int i = row; i <= rowMax; i++)
+            {
+                matrix[i, colMax] = value;
+                value++;
+            }
+            colMax--; // Переходим на следующий столбец
+
+            // Заполняем элементы нижней строки справа налево
+            for (int i = colMax; i >= colMin; i--)
+            {
+                matrix[rowMax, i] = value;
+                value++;
+            }
+            rowMax--; // Переходим на предыдущую строку
+
+            // Заполняем элементы левого столбца снизу вверх
+            for (int i = rowMax; i >= rowMin; i--)
+            {
+                matrix[i, colMin] = value;
+                value++;
+            }
+            colMin++; // Переходим на следующий столбец
+        }
+
+        // Выводим матрицу на консоль
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
     }
 }
-double diff = max - min;
-Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {diff}");
