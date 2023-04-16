5a14b9de9a044de047496bc3d2bb92a197347dc3
Aleksandr Dimitrash <fireboard@me.com>
2023-04-16 16:34:12 +0300

    Скопировал из заготовок

---
+++ Seminar7/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿double[] arr = {1.2, 3.4, 5.6, 7.8, 9.0};
+double min = arr[0];
+double max = arr[0];
+for(int i = 1; i < arr.Length; i++)
+{
+    if(arr[i] < min)
+    {
+        min = arr[i];
+    }
+    else if(arr[i] > max)
+    {
+        max = arr[i];
+    }
+}
+double diff = max - min;
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {diff}");
